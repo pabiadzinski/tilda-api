c6dce78f135fdbee354ad75536b8722143955442
pabiadzinski <pabiadzinski@gmail.com>
2019-01-24 22:34:21 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tildaapi',
+    version='0.1',
+    url='https://github.com/pabiadzinski/tilda-api',
+    description='',
+    author='pabiadzinski',
+    author_email='pabiadzinski@gmail.ru',
+    license='MIT',
+    packages=['tildaapi']
+)
